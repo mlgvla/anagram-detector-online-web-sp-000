ba711ac989ed1262ff0f5c5eb89ebe8747fec549
Monica Gerard <mlgerardvla@gmail.com>
2020-03-20 15:28:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class Anagram
   attr_accessor :word
 
@@ -7,6 +7,30 @@ class Anagram
   end
 
   def match(array)
-    array.select {|w| w == @word}
+    no_match = []
+    if no_match ==  array.select {|w| w == @word}
+      binding.pry
+      no_match
+    else
+      array.select do |w|
+        binding.pry
+        alpha_string(w) == alpha_string(@word)
+      end
+    end
+  end
+
+  def alpha_string(word)
+     word.split("").sort.join
   end
 end
+
+# Sandbox
+#
+# word = "listen"
+# array = %w(enlists google inlets banana)
+#
+# def alpha_string(word)
+#    word.split("").sort.join
+# end
+#
+# array.select {|w| w if alpha_string(w) == alpha_string(word)}
